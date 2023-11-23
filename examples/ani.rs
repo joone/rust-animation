b846625ba302f6cf34a2cd27c470b53c2dac604f
Joone Hur <joone.hur@gmail.com>
2023-11-23 00:32:17 -0800

    Update copyright email address

--- examples/ani.rs
+++ examples/ani.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Joone Hur <joone@kldp.org> All rights reserved.
+// Copyright (c) 2021 Joone Hur <joone@chromium.org> All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
